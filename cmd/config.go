1e2261e5aed990b2c69a34fd1a7cd481c26543f9
Erick Suarez <ericksuarezdev@gmail.com>
2022-08-12 14:50:06 -0500

    minor fix

--- cmd/config.go
+++ cmd/config.go
@@ -1,17 +1,11 @@
 package cmd
 
 import (
-	"gopkg.in/yaml.v3"
-	"log"
-	"os"
-
 	"github.com/esvarez/togo/config"
 	"github.com/spf13/cobra"
 )
 
 var defaultList string
-
-const path = "./config/config.yaml"
 
 func newConfigCmd(conf *config.Configuration) *cobra.Command {
 	c := &cobra.Command{
@@ -20,20 +14,7 @@ func newConfigCmd(conf *config.Configuration) *cobra.Command {
 		Long:  `Configure the application.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			conf.App.DefaultList = defaultList
-			f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-			if err != nil {
-				log.Fatalf("Unable to cache oauth token: %v", err)
-			}
-			defer f.Close()
-			bytes, err := yaml.Marshal(conf)
-			if err != nil {
-				log.Fatalf("Unable to cache oauth token: %v", err)
-			}
-
-			_, err = f.Write(bytes)
-			if err != nil {
-				log.Fatalf("Unable to cache oauth token: %v", err)
-			}
+			config.SaveConfiguration(conf)
 		},
 	}
 
